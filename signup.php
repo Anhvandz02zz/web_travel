4f5c7467e11b6d38a9029b6cdeb6716b564a3005
Tran Anh Van <anhvandz02@gmail.com>
2022-04-09 10:09:51 +0700

    signup

--- signup.php
+++ signup.php
@@ -70,9 +70,9 @@
     </form>
   </div>
      <?php
-     $servername = "localhost";
-     $username ="root";
-     $password="";
+     $servername = "3.132.234.157";
+     $username ="anhvan";
+     $password="123@123a";
      $database = "web_travel";
   //Khai báo biến để kết nối đến CSDL
      $connect = mysqli_connect($servername, $username,$password,$database);
